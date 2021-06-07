c0fb078ded9a363477c33d12418537f6b6873461
Andrew Pellum <apellum19@gmail.com>
2021-06-07 12:49:28 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = 'Drew Pellum'; 
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)} ${currentUser.slice(5)}!`;
+
